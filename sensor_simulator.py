fc943795078a2780392e5da971dfff2a54fbdb04
sp15442 <sp15442@FWWS23387637.saipemnet.saipem.intranet>
2012-11-21 16:28:32 +0100

    01.08.48

--- sensor_simulator.py
+++ sensor_simulator.py
@@ -47,10 +47,10 @@ class Sensor_Simulator(sensor.Sensor):
             globalvars.meteo_data.hum_in  = random.randrange(1,100)     
             globalvars.meteo_data.temp_in  = random.randrange(1,100)    
             globalvars.meteo_data.hum_out  = random.randrange(1,100)    
-            globalvars.meteo_data.temp_out   =random.randrange(1,100)  
-            globalvars.meteo_data.abs_pressure = random.randrange(1,100)
-            globalvars.meteo_data.wind_ave     = random.randrange(1,100)
-            globalvars.meteo_data.wind_gust    = random.randrange(1,100)
+            globalvars.meteo_data.temp_out   =random.randrange(-5,38)  
+            globalvars.meteo_data.abs_pressure = random.randrange(950,1030)
+            globalvars.meteo_data.wind_ave     = random.randrange(1,50)
+            globalvars.meteo_data.wind_gust    = random.randrange(50,100)
             globalvars.meteo_data.wind_dir     = random.randrange(1,360)
             globalvars.meteo_data.wind_dir_code = random.choice(['N','S','E','W','NW','NE','SW','SE'])
             globalvars.meteo_data.rain  = random.randrange(1,100)      
@@ -59,7 +59,6 @@ class Sensor_Simulator(sensor.Sensor):
             
      
             globalvars.meteo_data.CalcStatistics()
-            
             globalvars.meteo_data.LogDataToDB()
             
 
