3d1267368b9cbdacf1172142835082e6b703fd61
tony <tony.tarsi@gmail.com>
2014-09-03 10:18:04 +0200

    01.22.24

--- web_server.py
+++ web_server.py
@@ -32,6 +32,7 @@ chars = string.ascii_letters + string.digits
 sessionDict = {} # dictionary mapping session id's to session objects
 
 _enabled_path = ("/jscolor/","/reset_camera.py","/","/index.html","/login.py","/swpi_webconfig.py","/swpi-banner.jpg","/log/","/db/","/download_cfg.py","/swpi.cfg","/upload_cfg.py","/upload_cfg.html","/web_reboot.py","/web_status.py","/web_swpi_update.py","/favicon.ico")
+#_enabled_path = ("/reset_camera.py","/","/index.html","/login.py","/swpi_webconfig.py","/swpi-banner.jpg","/log/","/db/","/download_cfg.py","/upload_cfg.py","/upload_cfg.html","/web_reboot.py","/web_status.py","/web_swpi_update.py","/favicon.ico")
 
 
 class SessionElement(object):
