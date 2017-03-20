875ffee1e6896fb44f0ff413687bcb4b2f021699
ovrheat <nik@nik.wifi4all.it>
2017-03-20 23:01:48 +0100

    add support for FineOffset WH3080 (rtl-sdr)

--- sensor.py
+++ sensor.py
@@ -36,7 +36,7 @@ class Sensor(threading.Thread):
 	
 	def __init__(self ,cfg):
 		self.cfg = cfg
-		self.implementedStations = ["WH1080-RFM01","WH1080_RTL-SDR"]
+		self.implementedStations = ["WH1080-RFM01","WH1080_RTL-SDR","WH3080_RTL-SDR"]
 		#self.implementedStations = ["SIMULATE","PCE-FWS20","NEVIO8","NEVIO16","PCE-SENSOR","DAVIS-SENSOR","LACROSS-TX23","WMR100","WMR200","WMR918","WM918","WH1080-RFM01"]
 		
 #		if ( self.cfg.sensor_type not in self.implementedStations  ):
