7b2b27902e0e28a150ea672fb4701598c13c3b67
ToninoTarsi <tony.tarsi@gmail.com>
2018-01-10 21:48:25 +0100

    01.25.25

--- BME280.py
+++ BME280.py
@@ -87,63 +87,73 @@ class BME280():
 				self.digH[i] = (-self.digH[i] ^ 0xFFFF) + 1  
 
 	def read_temperature(self):
-		global t_fine
-		data = []
-		for i in range (0xF7, 0xF7+8):
-			data.append(self.bus.read_byte_data(self.i2c_address,i))
-		temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
-		v1 = (temp_raw / 16384.0 - self.digT[0] / 1024.0) * self.digT[1]
-		v2 = (temp_raw / 131072.0 - self.digT[0] / 8192.0) * (temp_raw / 131072.0 - self.digT[0] / 8192.0) * self.digT[2]
-		t_fine = v1 + v2
-		temperature = t_fine / 5120.0
-		return temperature
+		try:
+			global t_fine
+			data = []
+			for i in range (0xF7, 0xF7+8):
+				data.append(self.bus.read_byte_data(self.i2c_address,i))
+			temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+			v1 = (temp_raw / 16384.0 - self.digT[0] / 1024.0) * self.digT[1]
+			v2 = (temp_raw / 131072.0 - self.digT[0] / 8192.0) * (temp_raw / 131072.0 - self.digT[0] / 8192.0) * self.digT[2]
+			t_fine = v1 + v2
+			temperature = t_fine / 5120.0
+			return temperature
+		except :
+			return None
 
 
 	def read_humidity(self):
 		global t_fine
-		data = []
-		for i in range (0xF7, 0xF7+8):
-			data.append(self.bus.read_byte_data(self.i2c_address,i))
-		hum_raw  = (data[6] << 8)  |  data[7]
-		var_h = t_fine - 76800.0
-		if var_h != 0:
-			var_h = (hum_raw - (self.digH[3] * 64.0 + self.digH[4]/16384.0 * var_h)) * (self.digH[1] / 65536.0 * (1.0 + self.digH[5] / 67108864.0 * var_h * (1.0 + self.digH[2] / 67108864.0 * var_h)))
-		else:
-			return 0
-		var_h = var_h * (1.0 - self.digH[0] * var_h / 524288.0)
-		if var_h > 100.0:
-			var_h = 100.0
-		elif var_h < 0.0:
-			var_h = 0.0
-	
-		return var_h
-
+		try:
+			data = []
+			for i in range (0xF7, 0xF7+8):
+				data.append(self.bus.read_byte_data(self.i2c_address,i))
+			hum_raw  = (data[6] << 8)  |  data[7]
+			var_h = t_fine - 76800.0
+			if var_h != 0:
+				var_h = (hum_raw - (self.digH[3] * 64.0 + self.digH[4]/16384.0 * var_h)) * (self.digH[1] / 65536.0 * (1.0 + self.digH[5] / 67108864.0 * var_h * (1.0 + self.digH[2] / 67108864.0 * var_h)))
+			else:
+				return 0
+			var_h = var_h * (1.0 - self.digH[0] * var_h / 524288.0)
+			if var_h > 100.0:
+				var_h = 100.0
+			elif var_h < 0.0:
+				var_h = 0.0
+		
+			return var_h
+		except :
+			return None
+		
 	def read_pressure(self):
 		global t_fine
 
-		data = []
-		for i in range (0xF7, 0xF7+8):
-			data.append(self.bus.read_byte_data(self.i2c_address,i))
-		pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
-		pressure = 0.0
-		v1 = (t_fine / 2.0) - 64000.0
-		v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * self.digP[5]
-		v2 = v2 + ((v1 * self.digP[4]) * 2.0)
-		v2 = (v2 / 4.0) + (self.digP[3] * 65536.0)
-		v1 = (((self.digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((self.digP[1] * v1) / 2.0)) / 262144
-		v1 = ((32768 + v1) * self.digP[0]) / 32768
-	
-		if v1 == 0:
-			return 0
-		pressure = ((1048576 - pres_raw) - (v2 / 4096)) * 3125
-		if pressure < 0x80000000:
-			pressure = (pressure * 2.0) / v1
-		else:
-			pressure = (pressure / v1) * 2
-		v1 = (self.digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
-		v2 = ((pressure / 4.0) * self.digP[7]) / 8192.0
-		pressure = pressure + ((v1 + v2 + self.digP[6]) / 16.0)  
-		return pressure
+
+		try:
+			data = []
+			for i in range (0xF7, 0xF7+8):
+				data.append(self.bus.read_byte_data(self.i2c_address,i))
+			pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+			pressure = 0.0
+			v1 = (t_fine / 2.0) - 64000.0
+			v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * self.digP[5]
+			v2 = v2 + ((v1 * self.digP[4]) * 2.0)
+			v2 = (v2 / 4.0) + (self.digP[3] * 65536.0)
+			v1 = (((self.digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((self.digP[1] * v1) / 2.0)) / 262144
+			v1 = ((32768 + v1) * self.digP[0]) / 32768
+		
+			if v1 == 0:
+				return 0
+			pressure = ((1048576 - pres_raw) - (v2 / 4096)) * 3125
+			if pressure < 0x80000000:
+				pressure = (pressure * 2.0) / v1
+			else:
+				pressure = (pressure / v1) * 2
+			v1 = (self.digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
+			v2 = ((pressure / 4.0) * self.digP[7]) / 8192.0
+			pressure = pressure + ((v1 + v2 + self.digP[6]) / 16.0)  
+			return pressure
+		except :
+			return None
 
 
 if __name__ == '__main__':
